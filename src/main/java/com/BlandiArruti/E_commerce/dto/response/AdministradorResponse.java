df3ddb38a12ff077441c2af1472f928413cec855
AlanArruti <arrutialan@gmail.com>
2026-05-07 13:56:51 -0300

    add_services
    Co-Authored-By: iarablandi316 <230788600+iarablandi316@users.noreply.github.com>

---
+++ src/main/java/com/BlandiArruti/E_commerce/dto/response/AdministradorResponse.java
@@ -0,0 +1,6 @@
+package com.BlandiArruti.E_commerce.dto.response;
+
+public record AdministradorResponse(
+        Long idAdministrador,
+        String username
+) {}
--- src/main/java/com/BlandiArruti/E_commerce/enums/EstadoPedido.java
+++ src/main/java/com/BlandiArruti/E_commerce/enums/EstadoPedido.java
@@ -1,6 +1,7 @@
 package com.BlandiArruti.E_commerce.enums;
 
 public enum EstadoPedido {
+    PENDIENTE_PAGO,
     PAGADO,
     EN_PREPARACION,
     DESPACHADO,
